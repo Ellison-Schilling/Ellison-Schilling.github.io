63962dd49483ee40c226623f817bc4dfc1729a60
Ellison Largent/Schilling <ellisons@uoregon.edu>
2025-02-03 14:11:56 -0800

    Updated

--- js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const mobileMenu = document.getElementById('mobile-menu');
-    const body = document.body;
-
-    mobileMenu.addEventListener('click', function() {
-        body.classList.toggle('menu-active');
-    });
-});
